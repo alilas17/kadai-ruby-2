b81ed165f0c77c5b61075f28f868c6cfd5fd2c58
alilas17 <1arisakusaku17@gmail.com>
2022-06-13 12:48:31 +0000

    second

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,17 @@
 num_max = 100
-(1..num_max).each do |num|
-  
+
 def fizzbuzz(num)
     if num % 15 == 0
-       puts "FizzBuzz"
+      "FizzBuzz"
     elsif num % 3 == 0
-        puts "Fizz"
+      "Fizz"
     elsif num % 5 ==0
-        puts "Buzz"
-    else 
-        puts num
+      "Buzz"
+    else
+      num
     end
 end
-puts fizzbuzz(num)
+
+(1..num_max).each do |num|
+  puts fizzbuzz(num)
 end
-    
